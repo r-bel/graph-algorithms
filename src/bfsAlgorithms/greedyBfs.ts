2805f212b18462ef3e0e18c856f45e704d6e7d98
Dirk Dhaenens <A0084824@dematic.com>
2023-04-07 16:54:26 +0200

    renames

--- src/bfsAlgorithms/greedyBfs.ts
+++ src/bfsAlgorithms/greedyBfs.ts
@@ -1,12 +1,11 @@
 import { bfsGeneric } from "./bfsGeneric";
 import { HeapBasedPriorityQueue } from "../dataStructures/heapBasedPriorityQueue";
 import { AdjacencyProvider } from "../generic-models/adjacencyProvider";
-import { ProvidesEdgeAttribute } from "../generic-models/providesEdgeAttribute";
 
 /**
  * Uniform Cost Search algorithm aka Dijkstra's algorithm
  */
-export const dijkstra = (graph: AdjacencyProvider & ProvidesEdgeAttribute<number, number>, start: number, destination?: number): Map<number, number | null> => {
+export const greedyBfs = (graph: AdjacencyProvider, heuristic: (x: number, y: number) => number, start: number, destination?: number): Map<number, number | null> => {
 
     const cameFrom = new Map<number, number | null>();
     const frontier = new HeapBasedPriorityQueue<number>();
@@ -16,14 +15,12 @@ export const dijkstra = (graph: AdjacencyProvider & ProvidesEdgeAttribute<number
     frontier.enqueue(start, 0);
     costSoFar.set(start, 0);
 
-    const heuristic = (a: number | undefined, b: number| undefined) => 1; // Math.abs();
-
     const evalNeighbor = (next: number, current: number | null) => {
 
         const hasReached = cameFrom.has(next);
 
         if (!(hasReached)) {
-            const priority = heuristic(destination, next);
+            const priority = heuristic(destination!, next);
             frontier.enqueue(next, priority);
             cameFrom.set(next, current);
         }
